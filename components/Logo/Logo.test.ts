10b068b4a4db46c54619ee319bf95eeb2c5af5f3
Francisco <javiermartincuenca@gmail.com>
2024-05-25 21:51:11 +0200

    Color black test

--- components/Logo/Logo.test.ts
+++ components/Logo/Logo.test.ts
@@ -3,9 +3,15 @@ import { screen, render } from '@testing-library/vue';
 import Logo from './Logo.vue';
 
 describe('<Logo/>', () => {
-  it('should render a white label y default', async () => {
+  it('should render a white label by default', async () => {
     render(Logo);
     expect(screen.getByLabelText(/Won Games/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Won Games/i)).toHaveClass('text-white');
+  });
+
+  it('should render a black label when black color is passed', async () => {
+    render(Logo, { props: { color: 'black' } });
+    expect(screen.getByLabelText(/Won Games/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Won Games/i)).toHaveClass('text-black');
   });
 });
