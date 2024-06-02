8ebb831fea389ec296e7af84d4642bdfeb514d70
Francisco <javiermartincuenca@gmail.com>
2024-06-02 19:20:12 +0200

    Add main for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,12 @@
 import type { StorybookConfig } from '@storybook-vue/nuxt';
 
 const config: StorybookConfig = {
-  stories: ['../components/**/*.stories.ts'],
-  addons: ['@storybook/addon-essentials'],
+  stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook-vue/nuxt',
     options: {},
