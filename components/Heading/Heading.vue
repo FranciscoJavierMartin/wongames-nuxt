fba0f2a025c3b1c69aead2b79b44a74a34c9d03d
Francisco <javiermartincuenca@gmail.com>
2024-06-02 19:02:20 +0200

    Add styles

--- components/Heading/Heading.vue
+++ components/Heading/Heading.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <h1><slot /></h1>
-    <small>{{ color }}</small>
-  </div>
+  <h2
+    class="text-xlarge"
+    :class="{
+      'text-white': color === 'white',
+      'text-black': color === 'black',
+      'border-l-[0.7rem] border-solid border-l-secondary pl-xxsmall': lineLeft,
+      'relative mb-medium after:absolute after:bottom-[-0.5rem] after:left-[0px] after:w-[5rem] after:border-b-[0.4rem] after:border-solid after:border-b-primary':
+        lineBottom,
+    }"
+  >
+    <slot />
+  </h2>
 </template>
 
 <script setup lang="ts">
@@ -13,9 +21,19 @@ type HeadingProps = {
    * Text color
    */
   color?: 'white' | 'black';
+  /**
+   * Add line to left
+   */
+  lineLeft?: boolean;
+  /**
+   * Add line to bottom
+   */
+  lineBottom?: boolean;
 };
 
 withDefaults(defineProps<HeadingProps>(), {
   color: 'black',
+  lineLeft: false,
+  lineBottom: false,
 });
 </script>
